306adb09b979c8ba77e7e20c55d45c8656412f76
nazamoresco <nazamoresco99@gmail.com>
2022-06-11 18:18:25 -0300

    First commit :)

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DEU',
+      theme: ThemeData(),
+      home: const Scaffold(),
+    );
+  }
+}
